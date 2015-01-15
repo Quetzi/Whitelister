210a8eff5b4b4c7a42c943357fdb1fd0c869a545
Quetzi <q@quetzi.net>
2015-01-15 11:58:21 +0000

    Fix command access levels

--- src/main/java/net/quetzi/whitelister/commands/CommandWhitelist.java
+++ src/main/java/net/quetzi/whitelister/commands/CommandWhitelist.java
@@ -92,26 +92,8 @@ public class CommandWhitelist extends CommandBase {
 
     }
 
-    @Override
-    public boolean canCommandSenderUse(ICommandSender var1) {
-
-        return true;
-    }
-
-    @Override
-    public List addTabCompletionOptions(ICommandSender var1, String[] var2, BlockPos pos) {
-
-        return null;
-    }
-
-    @Override
-    public boolean isUsernameIndex(String[] var1, int var2) {
-
-        return false;
-    }
-
     public int getRequiredPermissionLevel() {
 
-        return 4;
+        return 3;
     }
 }
